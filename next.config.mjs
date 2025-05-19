be510897b248875bf56cae9cc2bd041c65213fe8
Giuseppe Maiarù <giuseppe.maiaru@techspirit.co>
2025-05-19 20:27:30 +0400

    static exp

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
